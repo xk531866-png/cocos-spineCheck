f7c2f060399ec36918256095aa87b418e06f0875
david <1522490632@qq.com>
2026-01-09 16:11:56 +0800

    feat:打包

---
+++ build/web-mobile/src/settings.11ce4.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/gmae.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",main:"ab35c"}};
